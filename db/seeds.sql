44d4b727af6c157b73ced022daccd4b2c168f0b6
ReenaH <Reena_Hunjan@yahoo.com>
2022-07-23 20:46:14 -0400

    initial queries and route

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,12 @@ VALUES
     ('Finance'),
     ('Strategy'),
     ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Manager', 80000, 4), 
+        ('Senior Manager', 100000, 4), 
+        ('Head', 200000, 4);
+
    
+
+
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,11 +8,65 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
+// Connect to database
+    const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '!Apple@SQL',
+    database: 'company'
+  },
+  console.log('Connected to the company database.')
+);
+
+//Query showing department names and department ids
+
+app.get('/api/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+
+db.query(sql, (err, rows) => {
+    if (err){
+        res.status(500).json({error: err.message});
+        return;
+    }
   res.json({
-    message: 'Hello World'
-  });
+    message: 'sucess',
+    data: rows
+      });
+    });
 });
+
+
+
+
+
+//Query to add department
+    const sql = `INSERT INTO departments (name) VALUES (?)`;
+    const params = ['Marketing'];
+
+    db.query(sql, params, (err, result) => {
+        if (err){
+            console.log(err)
+        } 
+       // console.log(result);
+    });
+
+//Query to delete a department
+db.query(`DELETE FROM departments WHERE id = ?`, 1, (err, result) => {
+    if (err){
+        console.log(err);
+    } 
+   // console.log(result);
+});
+
+//query to view all roles
+db.query(`SELECT * FROM roles`, (err, rows) => {
+  //  console.log(rows);
+});
+
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
